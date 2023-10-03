1786f7bbc10a1e2bacdda5bbff53a8df73322b31
Olafoyeku-josiah <145551656+Olafoyeku-josiah@users.noreply.github.com>
2023-10-03 05:15:52 -0700

    little changes

--- augmented_operators-practice.py
+++ augmented_operators-practice.py
@@ -1,7 +1,7 @@
-def class_schedule():
-    """this functions asks the user for the total number of students in class
+"""def class_schedule():
+    this functions asks the user for the total number of students in class
     conditions were set that if number of students are 1 or 2 then the user(instructor)
-    should wait for about 30 minutes for other students before he can start the class"""
+    should wait for about 30 minutes for other students before he can start the class
     print("This is a function which helps the user decide if the class should start with a certain number of students in class or not")
     total_students=int(input(" Enter the number of students present in python class currently: "))
     time_frame=input(" Enter the time for the python class(e.g 10:00): ")
@@ -10,12 +10,25 @@ def class_schedule():
         after_30mins=int(input( "if after 30 mins, Enter the number of students that entered: "))
         if after_30mins:
             after_30mins+=total_students
-            """in line 13 the addition augmented operator was used to calculate total students
-            in class after 30 mins through the use of the nested if conditional statement"""
+            in line 13 the addition augmented operator was used to calculate total students
+            in class after 30 mins through the use of the nested if conditional statement
             print(" Since after 30 mins total number of students in class is {} ,you can start the class properly. ".format(after_30mins))
     elif total_students in range(3,6) and time_frame==time_frame:
         print(" since total students in class is greater than 2 ,You can start the class properly and look at pending assignments")
     else:
         print("Reach out to the rest of the students.")
     #return after_30mins,total_students
-
+    """
+"""while True:
+    try:
+        x=int(input("please enter a number: "))
+        break
+    except ValueError:
+        print("oops! that was no valid number ...try again..")"""
+while True:
+    try:
+        x=int(input("pls enter a number: "))
+        print("the number you inputted is : {}".format(x))
+        
+    except ValueError:
+        print("oops!....thats not a valid number ....try again")
