6c1be2429ddefa46c2b882f7450a227c2ac21814
Olafoyeku-josiah <145551656+Olafoyeku-josiah@users.noreply.github.com>
2023-10-29 14:11:30 -0700

    Erased a code with error

--- functions_practice1.py
+++ functions_practice1.py
@@ -1,36 +1,3 @@
-#multiple functions to calculate total volume of three solid shapes..
-"""def name(user_name):
-    user_name=input("Enter your name: ")
-    print("HI {}".format(user_name))
-name(name)
-
-def first_volume(length,height,breadth):
-    length=input(" Enter the length of the first solid shape: ")
-    height=input(" Enter the height for the first solid shape: ")
-    breadth=input(" Enter the breadth for the first soild shape: ")
-    volume=int(length)*int(height)*int(breadth)
-    return volume
-print("")
-
-def second_volume(length,height,breadth):
-    length=input(" Enter the length of the second solid shape: ")
-    height=input(" Enter the height for the second solid shape: ")
-    breadth=input(" Enter the breadth for the second soild shape: ")
-    volume=int(length)*int(height)*int(breadth)
-    return volume
-print("")
-
-def third_volume(length,height,breadth):
-    length=input(" Enter the length of the third solid shape: ")
-    height=input(" Enter the height for the third solid shape: ")
-    breadth=input(" Enter the breadth for the third soild shape: ")
-    volume=int(length)*int(height)*int(breadth)
-    return volume
-print("")"""
-
-
-
-
 def addition():
     global num1
     global num2
